336ac1154bf0b3645f77ed2c98c2812264fa452e
Aaron Danton <aarondanton04@hotmail.com>
2026-06-22 18:21:07 +0100

    feat: add additional project euler solutions

--- tests/examples/project_euler/problems.rs
+++ tests/examples/project_euler/problems.rs
@@ -9,3 +9,23 @@ fn problem_1() {
         .success()
         .stdout("233168\n");
 }
+
+#[test]
+fn problem_2() {
+    Command::cargo_bin("concat")
+        .unwrap()
+        .arg("examples/projectEuler/problem2.concat")
+        .assert()
+        .success()
+        .stdout("4613732\n");
+}
+
+#[test]
+fn problem_4() {
+    Command::cargo_bin("concat")
+        .unwrap()
+        .arg("examples/projectEuler/problem4.concat")
+        .assert()
+        .success()
+        .stdout("906609\n");
+}
