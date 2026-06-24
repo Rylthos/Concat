a2d58cacf4ced072cbe336e35fe74ff23d6fa362
Aaron Danton <aarondanton04@hotmail.com>
2026-06-24 23:29:54 +0100

    feat: implement bubble sort

--- tests/examples/examples.rs
+++ tests/examples/examples.rs
@@ -105,3 +105,17 @@ FizzBuzz
 "#,
         );
 }
+
+#[test]
+fn example_bubblesort() {
+    Command::cargo_bin("concat")
+        .unwrap()
+        .arg("examples/bubble_sort.concat")
+        .assert()
+        .success()
+        .stdout(
+            r#"Before: 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+After : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+"#,
+        );
+}
